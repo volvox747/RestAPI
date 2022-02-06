4abb7a1aba27bbaff88852c7cdf2387620833973
Benson Thomas <bensonthomascr7@gmail.com>
2022-02-07 01:55:43 +0530

    Connected MongoDB database as a DaaS(DataBase as a service) using mongoose ODM

--- RestAPI/index.js
+++ RestAPI/index.js
@@ -1,11 +1,28 @@
-const express=require('express');
-const mongoose=require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
-const app=express();
+const app = express();
 
 
-app.get('/',(req,res)=>{
-    res.send("Hello World");
-})
+//^ Connecting to MongoDB database using "MongoDB Atlas" 
+mongoose
+  .connect(
+    "mongodb+srv://volvox747:Bensoncr7@rest.rp0gv.mongodb.net/restapi?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Hurray!! Connection Successfull.");
+  })
+  .catch((e) => {
+    console.log(new Error(e).message);
+  });
 
-app.listen(8080,()=>{console.log("Listening to port 8080");})
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+//^ This is how you connect express
+app.listen(8080, () => {
+  console.log("Listening to port 8080");
+});
