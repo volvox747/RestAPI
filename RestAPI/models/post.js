7af498d57d5d24611d6455ac1decee35d63dd2e7
Benson Thomas <bensonthomascr7@gmail.com>
2022-02-07 14:45:53 +0530

    Created a "models" folder in that added a "post.js" file and inside the post file created a "Schema" for post using mongoose

---
+++ RestAPI/models/post.js
@@ -0,0 +1,13 @@
+
+//^ This file was crwated to create Schemas(prototypes or models) of database 
+const mongoose=require('mongoose');
+
+//^ creating Schema 
+const Post=mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:
+})
+
